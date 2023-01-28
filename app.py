cc493676105f0c68ec8588eee72dcd2aae3e4a19
Diego Sarmanho <68619825+sarmanhodev@users.noreply.github.com>
2023-01-28 11:35:11 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import requests
+from flask import*
+
+api_key="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"
+endpoint="https://api.openai.com/v1/completions"
+
+
+
+app = Flask(__name__,template_folder="template")
+
+@app.route("/home", methods=['GET', 'POST'])
+def pesquisa():
+
+    if request.method =='POST':
+        busca = request.form['busca']
+
+        
+        model = "text-davinci-003"
+
+        data={
+            "prompt": busca,
+            "model":model,
+            "max_tokens":1000
+        }
+
+        response= requests.post(endpoint, json=data, headers={
+            "Content-Type":"application/json",
+            "Authorization":f"Bearer {api_key}"
+        })
+
+        res = response.json()
+        texto = res['choices'][0]['text']
+
+        print(texto)
+
+        return render_template("home.html", busca = busca, texto=texto)
+
+
+    return render_template("home.html")
+    
+
+if __name__=="__main__":
+    app.run(debug=True, host="0.0.0.0")
